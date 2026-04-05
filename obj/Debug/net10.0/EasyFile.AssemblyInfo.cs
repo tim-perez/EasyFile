bf369a096dee4abfef592e7d3cc355ed35605333
Tim Perez <timperez624@gmail.com>
2026-04-04 23:49:25 -0700

    Small Updates (obj)

--- obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
+++ obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ac443e2a7ce5da9a58afba243b267a74b14321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b430d1e665fac61eb66b47561edeec28ba284c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
