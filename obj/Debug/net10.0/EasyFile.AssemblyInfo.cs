e631405147114b2f054789bde3799664c6f9616c
Tim Perez <timperez624@gmail.com>
2026-05-02 13:34:19 -0700

    obj

--- obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
+++ obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e60fd2a309f415cb07772e18700647806bde0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d7b0c8095f929a7e24ad9916a2e7f5751b059b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
