53522830fda1230e118287f178a6106a094fb6e4
Tim Perez <timperez624@gmail.com>
2026-05-02 00:31:22 -0700

    Updates

--- obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
+++ obj/Debug/net10.0/EasyFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3268f81b021e3017ae031d6a3fdff4832b4da865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c887f45fb57b71e052af1ac133b8117e3083ae64")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
